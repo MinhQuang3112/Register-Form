8794387570e22d6b4689ef074e819ff5ba3c73e3
MinhQuang3112 <89634314+MinhQuang3112@users.noreply.github.com>
2022-03-11 14:35:09 +0700

    add allfile

---
+++ main.js
@@ -0,0 +1,80 @@
+var userName=document.querySelector('#username')
+var email=document.querySelector('#email')
+var password=document.querySelector('#password')
+var password2=document.querySelector('#password2')
+var btnLogin=document.querySelector('.btn-login')
+var form=document.querySelector('form')
+console.log(form)
+function showError(input,message) {
+    let parent=input.parentElement
+    let small=parent.querySelector('small')
+    parent.classList.add('error')
+    small.innerText = message
+}
+function showSuccess(input) {
+    let parent=input.parentElement
+    let small=parent.querySelector('small')
+    parent.classList.remove('error')
+    parent.classList.add('success')
+    small.innerText =''
+}
+function checkEmptyError(listInput){
+    let ischeckEmptyError=false
+    listInput.forEach(input=>{
+        input.value=input.value.trim()
+         if(!input.value){
+             showError(input,`${getFieldname(input)} is required`)
+             ischeckEmptyError=true
+         }
+         else{
+             showSuccess(input)
+            }
+    })
+    return ischeckEmptyError
+
+}
+
+function checkLength(input,min,max){
+    if(input.value.length < min){
+        showError(input,`${getFieldname(input)} must be at least${min} characters`)
+    }
+    else if(input.value.length > max){
+        showError(input,`${getFieldname(input)} must be less than ${max} characters`)
+    }
+    else{
+        showSuccess(input)
+    }
+}
+function checkEmail(input){
+    const regexEmail =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if(regexEmail.test(input.value.trim())){
+        showSuccess(input)
+    }
+    else{
+        showError(input,'Email is not valid')
+    }
+    let a= input.value.trim().match(regexEmail)
+    console.log(a[0])
+}
+
+function checkPasswordMatch(input1,input2){ 
+    if(input1.value!==input2.value){
+        showError(input2,'Passwords do not match')
+    }   
+}   
+
+function getFieldname(input){
+    // viết hoa chữ cái đầu
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    if(!checkEmptyError([userName,email,password,password2])){
+        checkLength(userName,3,12)
+        checkLength(password,6,15)
+        checkEmail(email)
+        checkPasswordMatch(password,password2)
+    }
+})
